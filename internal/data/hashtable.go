1827aa4fefa0218d9322399c90d9a55bdd324c15
Yuriy Pertsev <stape.a@gmail.com>
2025-01-24 23:44:10 +0300

    Update hashtable.go
    Для того чтобы данные при перегрузки ноды сохранялись не в открытом json виде используем шифрование  
    internal/data/data.go:
      •  Добавили поле key для хранения ключа шифрования.
      •  Функция NewHashTable теперь принимает ключ шифрования.
      •  Добавили методы encryptData и decryptData для шифрования и расшифрования данных с использованием AES.
      •  Изменили SaveData и LoadData, чтобы использовать шифрование и расшифрование.

--- internal/data/hashtable.go
+++ internal/data/hashtable.go
@@ -1,96 +1,142 @@
 package data
 
 import (
+ "crypto/aes"
+ "crypto/cipher"
+ "crypto/rand"
  "encoding/json"
  "errors"
- "fmt"
- "io/ioutil"
+ "io"
  "os"
  "sync"
 )
 
-var (
- ErrKeyExists = errors.New("key already exists")
-    ErrKeyNotFound = errors.New("key not found")
-)
+var ErrKeyExists = errors.New("key already exists")
+
 type HashTable struct {
- data map[string]bool
-    mu  sync.RWMutex
+ data     map[string]bool
+ mu       sync.RWMutex
+ filename string
+ key      []byte // Ключ шифрования
 }
 
-func NewHashTable() *HashTable {
-    return &HashTable{
-        data: make(map[string]bool),
-    }
+func NewHashTable(key string) *HashTable {
+ return &HashTable{
+  data: make(map[string]bool),
+  mu:   sync.RWMutex{},
+  key:  []byte(key), // Convert key to byte slice
+ }
 }
 
 func (ht *HashTable) Put(key string) error {
-    ht.mu.Lock()
-    defer ht.mu.Unlock()
-    if _, exists := ht.data[key]; exists {
-        return ErrKeyExists
-    }
-    ht.data[key] = true
-    return nil
+ ht.mu.Lock()
+ defer ht.mu.Unlock()
+ if _, exists := ht.data[key]; exists {
+  return ErrKeyExists
+ }
+ ht.data[key] = true
+ return nil
 }
 
 func (ht *HashTable) Get(key string) bool {
-     ht.mu.RLock()
-    defer ht.mu.RUnlock()
- _, exists := ht.data[key]
- return exists
-}
-func (ht *HashTable) Delete(key string) error {
-     ht.mu.Lock()
-    defer ht.mu.Unlock()
-     if _, exists := ht.data[key]; !exists {
-         return ErrKeyNotFound
-     }
-    delete(ht.data, key)
-    return nil
+ ht.mu.RLock()
+ defer ht.mu.RUnlock()
+ return ht.data[key]
 }
 
-func (ht *HashTable) LoadData(filename string) error {
-    ht.mu.Lock()
-    defer ht.mu.Unlock()
- file, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0644)
-    if err != nil {
-        return fmt.Errorf("error opening file: %w", err)
-    }
- defer file.Close()
-
- data, err := ioutil.ReadAll(file)
- if err != nil {
-  return fmt.Errorf("error reading file: %w", err)
- }
- if len(data) == 0 {
-  return nil
- }
-
- err = json.Unmarshal(data, &ht.data)
- if err != nil {
-  return fmt.Errorf("error unmarshaling data: %w", err)
- }
-    return nil
+func (ht *HashTable) Delete(key string) {
+ ht.mu.Lock()
+ defer ht.mu.Unlock()
+ delete(ht.data, key)
 }
 
 func (ht *HashTable) SaveData(filename string) error {
-    ht.mu.RLock()
-    defer ht.mu.RUnlock()
- file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-    if err != nil {
-        return fmt.Errorf("error opening file: %w", err)
-    }
+ ht.mu.RLock()
+ defer ht.mu.RUnlock()
+ file, err := os.Create(filename)
+ if err != nil {
+  return err
+ }
  defer file.Close()
 
-    jsonData, err := json.Marshal(ht.data)
+ // Шифруем данные
+ encryptedData, err := ht.encryptData()
+ if err != nil {
+  return err
+ }
+
+ encoder := json.NewEncoder(file)
+ if err := encoder.Encode(encryptedData); err != nil {
+  return err
+ }
+ return nil
+}
+
+func (ht *HashTable) LoadData(filename string) error {
+ ht.filename = filename
+ file, err := os.Open(filename)
+ if err != nil {
+  if os.IsNotExist(err) {
+   return nil // Файл не существует, это нормально
+  }
+  return err
+ }
+ defer file.Close()
+
+ var encryptedData []byte
+ decoder := json.NewDecoder(file)
+ if err := decoder.Decode(&encryptedData); err != nil {
+  return err
+ }
+ // Расшифровываем данные
+ err = ht.decryptData(encryptedData)
+ if err != nil {
+  return err
+ }
+ return nil
+}
+// encryptData шифрует данные
+func (ht *HashTable) encryptData() ([]byte, error) {
+    plaintext, err := json.Marshal(ht.data)
     if err != nil {
-  return fmt.Errorf("error marshaling data: %w", err)
-    }
-    _, err = file.Write(jsonData)
-    if err != nil {
-       return fmt.Errorf("error writing data to file: %w", err)
+        return nil, err
     }
 
+    block, err := aes.NewCipher(ht.key)
+    if err != nil {
+        return nil, err
+    }
+
+    ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+    iv := ciphertext[:aes.BlockSize]
+    if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+        return nil, err
+    }
+
+    stream := cipher.NewCFBEncrypter(block, iv)
+    stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+    return ciphertext, nil
+}
+// decryptData расшифровывает данные
+func (ht *HashTable) decryptData(ciphertext []byte) error {
+    block, err := aes.NewCipher(ht.key)
+    if err != nil {
+        return err
+    }
+
+    if len(ciphertext) < aes.BlockSize {
+        return errors.New("ciphertext is too short")
+    }
+    iv := ciphertext[:aes.BlockSize]
+    ciphertext = ciphertext[aes.BlockSize:]
+
+    stream := cipher.NewCFBDecrypter(block, iv)
+    plaintext := make([]byte, len(ciphertext))
+    stream.XORKeyStream(plaintext, ciphertext)
+
+    err = json.Unmarshal(plaintext, &ht.data)
+ if err != nil {
+  return err
+ }
     return nil
 }
